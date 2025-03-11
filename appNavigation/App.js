14441f2ef7e7bea9e6f8326755e02aeaac689502
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-03-11 15:56:49 -0300

    Update App.js

--- appNavigation/App.js
+++ appNavigation/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 //import Stack1 from "./navigation/DrawerNavigator";
-//import Stack2 from "./navigation/StackNavigator";
-import Stack3 from "./navigation/TabNavigator";
+import Stack2 from "./navigation/StackNavigator";
+//import Stack3 from "./navigation/TabNavigator";
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack3 />
+            <Stack2 />
         </NavigationContainer>
     );
 }
